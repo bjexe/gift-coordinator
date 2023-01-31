750db8316762b4599dc089dc165cd411e51648bb
bjexe <bensimms@knights.ucf.edu>
2023-01-31 15:48:52 -0500

    Added newPage

---
+++ pages/newPage.tsx
@@ -0,0 +1,11 @@
+export default function newPage() {
+    return (
+        <>
+        <div>
+            <h1>
+                This is a new page!
+            </h1>
+        </div>
+        </>
+    );
+}
