6128caebf15b30b7060006ceece70c4fe4f19ed2
bjexe <bensimms@knights.ucf.edu>
2023-01-31 10:58:40 -0500

    fix tailwind config

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,16 @@ export default function Home() {
   return (
     <>
       <Head>
-      <title>index</title>
-      <meta name="description" content="this is the index page"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1"/>
-      <link rel="icon" href="/favicon.ico"/>
+        <title>index</title>
+        <meta name="description" content="this is the index page"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
       <main>
         <div className="h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-800 via-gray-900 to-black text-white">
-          Hello, World!
+          <h1 className="text-3xl font-bold underline">
+            Hello, World!
+          </h1>
         </div>
       </main>
     </>
