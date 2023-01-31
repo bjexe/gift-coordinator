59d776620c7e7c02152358a83b6d354a002016e7
Benjamin Simms <48174605+bjexe@users.noreply.github.com>
2023-01-31 15:56:23 -0500

    Add a new page (#2)
    * Added newPage
    
    * edit content of the <h1> on newPage
    
    * remove superlative so

---
+++ pages/newPage.tsx
@@ -0,0 +1,11 @@
+export default function newPage() {
+    return (
+        <>
+        <div>
+            <h1>
+                This newPage is cool
+            </h1>
+        </div>
+        </>
+    );
+}
